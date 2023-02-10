3455be944720a721e3bd4de967eb89288150d525
Brayan opiyo <67815971+opiyobrayan@users.noreply.github.com>
2023-02-10 09:41:32 +0300

    Update settings.py

--- system/settings.py
+++ system/settings.py
@@ -136,3 +136,4 @@ CKEDITOR_UPLOAD_PATH = "uploads/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CSRF_TRUSTED_ORIGINS=['https://*thc-kenya-system.up.railway.app/']
